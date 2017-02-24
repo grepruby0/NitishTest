056fcd849ed652f84d59652358fff95a817f8bbe
Nitish Agrawal <grepruby0@gmail.com>
2017-02-24 14:29:56 +0530

    Add model to store data

---
+++ app/src/main/java/com/android/test/DataModel.java
@@ -0,0 +1,63 @@
+package com.android.test;
+
+/**
+ * Created by rails-dev on 24/2/17.
+ */
+
+public class DataModel {
+
+    String imagePath = "";
+    String userName = "";
+    String email = "";
+    String phoneNumber = "";
+    String dob = "";
+    String gender = "";
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
